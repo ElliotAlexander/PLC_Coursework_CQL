9beebefc0a7d0b4ab7abfe0ed3a088faad3b910f
Harry Brown <harryjgbrown@gmail.com>
2018-03-09 01:19:44 +0000

    changed type of equalities to [(Int, Int)] rather than Map Int Int so that we dont need to worry about key collisions

--- Main.hs
+++ Main.hs
@@ -22,9 +22,9 @@ main = do
     --return $ parseCalc tokens
 
 --tree of functions to list of functions
-getFunctions :: Func -> [(String, Func)]
-getFunctions (Function name args body next) = [(name, (FuncEnd name args body))] ++ getFunctions next
-getFunctions (FuncEnd name args body) = [(name, (FuncEnd name args body))]
+getFunctions :: Func -> [(String, Data)]
+getFunctions (Function name args body next) = [(name, DataFunc (FuncEnd name args body))] ++ getFunctions next
+getFunctions (FuncEnd name args body) = [(name, DataFunc (FuncEnd name args body))]
 
 --executes function
 execute :: String -> [(String, Data)] -> [(String, Func)] -> [(String, Data)]
@@ -57,10 +57,19 @@ getData name (x:xs)
 process :: [(String, Data)] -> Exp -> [(String, Data)]
 process vars (VarInt var int next) = process (update vars (var, DataInt int)) next
 process vars (VarVar var1 var2 next) = process (update vars (var1, getData var2 vars)) next
-process vars (VarFunc var func next) = process (update vars (var, DataFunc func)) next
+process vars (VarFunc _ (FuncEnd funcName args body) next) = process (update vars (funcName, (FuncEnd funcName args body))) next
 --process vars (IfElse) = 
 --process vars (If) = 
---process vars (FuncCall) = 
+process vars (FuncCall funcName args)
+    | getData funcName == (DataFunc (FuncEnd funcName _ _)) = updateArgs args vars $ process (matchArgs args (FuncEnd name funcArgs body)) body
+            | otherwise = error "function " + funcName + " does not exist" 
+        where (FuncEnd name funcArgs body) = getData funcName 
 process vars (Return) = vars
 --process vars (ReturnVar) = 
 --process vars (ReturnInt) = 
+
+matchArgs :: Args -> Func -> [(String, Data)] -> [(String, Data)]
+matchArgs (ArgEnd varName) (FuncEnd _ (ArgEnd varInFuncName) body) vars = [(varInFuncName, getData varName vars)]
+matchArgs (Arg varName next) (FuncEnd funcName (Arg varInFuncName next) body) vars = (varInFuncName, getData varName vars) : matchArgs next (FuncEnd funcName next body) vars
+    
+updateArgs :: Args -> [(String, Data)] -> [(String, Data)] -> [(String, Data)]
