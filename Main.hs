8a499d097adb878f182a4a14b5a0bcf5d398079b
Elliot Alexander <epurvis97@gmail.com>
2018-03-15 13:06:16 +0000

    Added data.foldable

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@ module Main where
     import Data.Map.Strict
     import Data.List
     import Control.Monad
+    import Data.Foldable
 
 
     -- Note that if youre trying to run this in GHCI, you'll need to enable
